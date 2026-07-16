97f67f40b6be43d88290aec71e383a888801410f
Rosela <rosela@users.noreply.github.com>
2026-07-16 13:58:44 +0700

    feat: add Railway deploy config, DATABASE_URL support, Procfile

--- koneksi.go
+++ koneksi.go
@@ -3,7 +3,9 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/url"
 	"os"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
@@ -15,13 +17,18 @@ func Koneksi() *sql.DB {
 		log.Println("Info: .env tidak ditemukan, menggunakan environment variable sistem")
 	}
 
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASS")
-	dbName := os.Getenv("DB_NAME")
+	var dsn string
+	if dbURL := os.Getenv("DATABASE_URL"); dbURL != "" {
+		dsn = parseMySQLURL(dbURL)
+	} else {
+		dbHost := os.Getenv("DB_HOST")
+		dbPort := os.Getenv("DB_PORT")
+		dbUser := os.Getenv("DB_USER")
+		dbPass := os.Getenv("DB_PASS")
+		dbName := os.Getenv("DB_NAME")
+		dsn = dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+	}
 
-	dsn := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal("Gagal membuka koneksi database: ", err)
@@ -32,4 +39,31 @@ func Koneksi() *sql.DB {
 	}
 
 	return db
+}
+
+func parseMySQLURL(rawURL string) string {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		log.Fatal("DATABASE_URL tidak valid: ", err)
+	}
+
+	user := u.User.Username()
+	pass, _ := u.User.Password()
+	host := u.Host
+	dbName := strings.TrimPrefix(u.Path, "/")
+
+	query := u.Query()
+	if query.Get("replicaSet") != "" {
+		query.Del("replicaSet")
+	}
+	if query.Get("tls") != "" {
+		query.Del("tls")
+	}
+	q := query.Encode()
+
+	dsn := user + ":" + pass + "@tcp(" + host + ")/" + dbName
+	if q != "" {
+		dsn += "?" + q
+	}
+	return dsn
 }
